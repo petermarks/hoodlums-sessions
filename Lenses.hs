b52dd2d2c4088b9d31b7d2688dc6bbe59ae1ef5f
Peter Marks <peter@puss-in-boots.(none)>
2013-01-10 19:53:50 +0000

    First Lens

--- Lenses.hs
+++ Lenses.hs
@@ -1,23 +1,43 @@
 module Lenses where
 
+import Prelude hiding ((.), id)
+import Control.Category
+
+data Lens s a = Lens (s -> a) ((a -> a) -> s -> s)
+
+instance Category Lens where
+  id = Lens id id
+  (Lens lg lm) . (Lens rg rm) = Lens (lg . rg) (rm . lm)
+
+(^.) :: s -> Lens s a -> a
+s ^. (Lens f _) = f s
+
+(%=) :: Lens s a -> (a -> a) -> s -> s
+((Lens _ m) %= f) s = m f s
+
+(^=) :: Lens s a -> a -> s -> s
+l ^= a = l %= (const a)
+
+infixl 8 ^.
+infixr 4 %=
+infixr 4 ^=
+
+
 data Point = Point {x :: Double, y :: Double}
 	deriving (Eq, Show)
+
+xLens :: Lens Point Double
+xLens = Lens x (\f p -> p{x= f (x p)})
+
+yLens :: Lens Point Double
+yLens = Lens y (\f p -> p{y= f (y p)})
 
 data Circle = Circle {origin::Point, radius :: Double}
   deriving (Eq, Show)
 
-setRadius :: Double -> Circle -> Circle
-setRadius r c = c{radius=r}
+radiusLens :: Lens Circle Double
+radiusLens = Lens radius (\f c -> c{radius=f (radius c)})
 
-setOrigin :: Point -> Circle -> Circle
-setOrigin p = modifyOrigin (const p)
-
-modifyOrigin :: (Point -> Point) -> Circle -> Circle
-modifyOrigin f c = c{origin=f (origin c)}
-
-setX :: Double -> Point -> Point
-setX v p = p{x=v}
-
-setY :: Double -> Point -> Point
-setY v p = p{y=v}
+originLens :: Lens Circle Point
+originLens = Lens origin (\f c -> c{origin=f (origin c)})
 
