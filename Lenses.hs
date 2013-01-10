b2ea8a272043157b53f1d4096c5fcc531fade697
Peter Marks <peter@puss-in-boots.(none)>
2013-01-10 19:09:19 +0000

    Record syntax

--- Lenses.hs
+++ Lenses.hs
@@ -8,3 +8,12 @@ data Circle = Circle {origin::Point, radius :: Double}
 
 setRadius :: Double -> Circle -> Circle
 setRadius r c = c{radius=r}
+
+setOrigin :: Point -> Circle -> Circle
+setOrigin p c = c{origin=p}
+
+setX :: Double -> Point -> Point
+setX v p = p{x=v}
+
+setY :: Double -> Point -> Point
+setY v p = p{y=v}
