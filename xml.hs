6ac8bea0b47b0c973dd56d42d8ac463363172e23
Peter Marks <peter@indigomail.net>
2013-03-14 19:37:31 +0000

    Switched to List monad

--- xml.hs
+++ xml.hs
@@ -1,6 +1,7 @@
 module XML where
 
 import Control.Applicative
+import Control.Monad
 import Data.Maybe
 import Text.XML.Light
 
@@ -9,3 +10,20 @@ load = fromJust . parseXMLDoc <$> readFile "album.xml"
 
 pp :: Element -> IO ()
 pp = putStrLn . ppElement
+
+descendants :: Element -> [Element]
+descendants = concatMap f . elChildren
+  where
+    f e = e : descendants e
+
+hasTag :: QName -> Element -> [Element]
+hasTag n e | n == elName e = [e]
+           | otherwise     = []
+
+qn :: String -> QName
+qn s = QName s (Just "http://musicbrainz.org/ns/mmd-2.0#") Nothing
+
+trackNames :: Element -> [String]
+trackNames e = do
+  e' <- descendants >=> hasTag (qn "title") $ e
+  return $ strContent e'
