6a6dc27d3a1acd090b4419e1ba33230e8b3f88a5
Peter Marks <peter@indigomail.net>
2013-03-14 19:52:41 +0000

    Switched to DList

--- xml.hs
+++ xml.hs
@@ -2,6 +2,7 @@ module XML where
 
 import Control.Applicative
 import Control.Monad
+import Data.DList
 import Data.Maybe
 import Text.XML.Light
 
@@ -11,19 +12,21 @@ load = fromJust . parseXMLDoc <$> readFile "album.xml"
 pp :: Element -> IO ()
 pp = putStrLn . ppElement
 
-descendants :: Element -> [Element]
-descendants = concatMap f . elChildren
-  where
-    f e = e : descendants e
+descendants :: Element -> DList Element
+descendants = fromList . elChildren >=> 
+                  (\e -> return e `mplus` descendants e)
 
-hasTag :: QName -> Element -> [Element]
-hasTag n e | n == elName e = [e]
-           | otherwise     = []
+hasTag :: QName -> Element -> DList Element
+hasTag n e | n == elName e = return e
+           | otherwise     = mzero
 
 qn :: String -> QName
 qn s = QName s (Just "http://musicbrainz.org/ns/mmd-2.0#") Nothing
 
-trackNames :: Element -> [String]
+trans :: (Element -> DList a) -> Element -> [a]
+trans t = toList . t
+
+trackNames :: Element -> DList String
 trackNames e = do
   e' <- descendants >=> hasTag (qn "title") $ e
   return $ strContent e'
