40cae9b2fb582f8ddb05f17333fdd12fc2316c01
Peter Marks <peter@indigomail.net>
2013-03-14 20:30:30 +0000

    Made XML output

--- xml.hs
+++ xml.hs
@@ -2,6 +2,7 @@
 
 module XML where
 
+import Prelude hiding (elem)
 import Control.Applicative
 import Control.Monad
 import Data.DList
@@ -12,8 +13,8 @@ import Text.XML.Light.Cursor
 load :: IO Element
 load = fromJust . parseXMLDoc <$> readFile "album.xml"
 
-pp :: Element -> IO ()
-pp = putStrLn . ppElement
+pp :: [Content] -> IO ()
+pp = mapM_ $ putStrLn . ppContent
 
 type Trans a = Cursor -> DList a
 
@@ -33,12 +34,19 @@ getText :: Trans String
 getText (current -> Elem e) = return $ strContent e
 getText _ = mzero
 
+elem :: QName -> DList Content -> DList Content
+elem n = return . Elem . node n . toList
+
+text :: String -> DList Content
+text s = return . Text $ CData CDataText s Nothing
+
 qn :: String -> QName
 qn s = QName s (Just "http://musicbrainz.org/ns/mmd-2.0#") Nothing
 
 trans :: Trans a -> Element -> [a]
 trans t = toList . t . fromElement
 
-trackNames :: Trans String
-trackNames =
-  descendants >=> hasTag (qn "title") >=> getText
+trackNames :: Trans Content
+trackNames e = elem (qn "titles") $ do
+  t <- descendants >=> hasTag (qn "title") >=> getText $ e
+  elem (qn "title") $ text t
