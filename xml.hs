7ec2eb1cb0d19a43072fd9203678df10129bd28f
Peter Marks <peter@indigomail.net>
2013-03-14 23:44:52 +0000

    A more point-free version

--- xml.hs
+++ xml.hs
@@ -16,37 +16,38 @@ load = fromJust . parseXMLDoc <$> readFile "album.xml"
 pp :: [Content] -> IO ()
 pp = mapM_ $ putStrLn . ppContent
 
-type Trans a = Cursor -> DList a
+type Trans a b = a -> DList b
 
-children :: Trans Cursor
+children :: Trans Cursor Cursor
 children (current -> Elem e) = 
   fromList $ fmap fromElement $ elChildren e
 children _ = mzero
 
-descendants :: Trans Cursor
+descendants :: Trans Cursor Cursor
 descendants = children >=> (\c -> return c `mplus` descendants c)
 
-hasTag :: QName -> Trans Cursor
+hasTag :: QName -> Trans Cursor Cursor
 hasTag n c@(current -> Elem e) | n == elName e = return c
 hasTag _ _ = mzero
 
-getText :: Trans String
+getText :: Trans Cursor String
 getText (current -> Elem e) = return $ strContent e
 getText _ = mzero
 
-elem :: QName -> DList Content -> DList Content
-elem n = return . Elem . node n . toList
+elem :: QName -> Trans a Content -> Trans a Content
+elem n c = return . Elem . node n . toList . c
 
-text :: String -> DList Content
+text :: Trans String Content
 text s = return . Text $ CData CDataText s Nothing
 
 qn :: String -> QName
 qn s = QName s (Just "http://musicbrainz.org/ns/mmd-2.0#") Nothing
 
-trans :: Trans a -> Element -> [a]
+trans :: Trans Cursor a -> Element -> [a]
 trans t = toList . t . fromElement
 
-trackNames :: Trans Content
-trackNames e = elem (qn "titles") $ do
-  t <- descendants >=> hasTag (qn "title") >=> getText $ e
-  elem (qn "title") $ text t
+trackNames :: Trans Cursor Content
+trackNames =
+  elem (qn "titles") $
+    descendants >=> hasTag (qn "title") >=> getText >=>
+      elem (qn "title") text
