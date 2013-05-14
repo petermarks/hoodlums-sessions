352931580fb9c0f7a0a90c4fb7bbde59c92a684b
Peter Marks <peter@indigomail.net>
2013-05-14 19:31:20 +0100

    Added shuffling

--- SetGame.hs
+++ SetGame.hs
@@ -1,5 +1,10 @@
 module SetGame where
 
+import Control.Applicative
+import Data.List
+import System.Random
+import Data.Ord
+
 data Colour = Red | Purple | Green       deriving (Eq, Ord, Enum, Bounded, Show)
 data Shape  = Diamond | Squiggle | Oval  deriving (Eq, Ord, Enum, Bounded, Show)
 data Fill   = Solid | Open | Stripe      deriving (Eq, Ord, Enum, Bounded, Show)
@@ -10,12 +15,22 @@ data Card = Card
   , shape  :: Shape
   , fill   :: Fill
   , number :: Number
-  } deriving (Eq, Ord, Show)
+  } deriving (Eq, Ord)
 
+instance Show Card where
+  show (Card c s f n) = unwords [show n, show c, show f, show s] 
 
 deck :: [Card]
-deck = [ Card c s f n | c <- allEnum, s <- allEnum, f <- allEnum, n <- allEnum]
+deck = Card <$> allEnum <*> allEnum <*> allEnum <*> allEnum
 
 allEnum :: (Enum a, Bounded a) => [a]
 allEnum = [minBound .. maxBound]
 
+shuffle :: RandomGen g => g -> [a] -> [a]
+shuffle g = map snd . sortBy (comparing fst) . zip (randoms g :: [Double])
+
+
+main :: IO()
+main = do
+    g <- newStdGen
+    mapM_ print (take 10 (shuffle g deck))
