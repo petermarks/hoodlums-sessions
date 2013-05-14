7ca1f10b80e43a540e307ecef4600da837d8d2f7
Peter Marks <peter@indigomail.net>
2013-05-14 20:10:48 +0100

    Implemented findSet

--- SetGame.hs
+++ SetGame.hs
@@ -4,6 +4,7 @@ import Control.Applicative
 import Data.List
 import System.Random
 import Data.Ord
+import Data.Maybe
 
 data Colour = Red | Purple | Green       deriving (Eq, Ord, Enum, Bounded, Show)
 data Shape  = Diamond | Squiggle | Oval  deriving (Eq, Ord, Enum, Bounded, Show)
@@ -42,9 +43,22 @@ isGood :: Eq a => (Card -> a) -> [Card]-> Bool
 isGood f cards = let uniq = length . nub $ map f cards 
                   in uniq == 1 || uniq == length cards
 
+findSet :: [Card] -> Maybe ( (Card, Card, Card), [Card] )
+findSet hand = listToMaybe
+                 [ ((c1, c2, c3), h3)
+                 | (c1, h1) <- pick hand
+                 , (c2, h2) <- pick h1
+                 , (c3, h3) <- pick h2
+                 , isSet c1 c2 c3 
+                 ] 
 
+pick :: [a] -> [(a, [a])]
+pick as = unfoldr f ([], as)
+  where f (_, []) = Nothing
+        f (before, a:as') = Just ((a, before ++ as') , (a:before, as'))
 
 main :: IO()
 main = do
     g <- newStdGen
     mapM_ print (take 10 (shuffle g deck))
+
