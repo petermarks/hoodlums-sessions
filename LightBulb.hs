872bae341c35025655d4c25c30e2d6002a3a96d7
Peter Marks <peter@indigomail.net>
2013-06-12 21:34:53 +0100

    Moved socket reading out of animate handler so as not to block UI

--- LightBulb.hs
+++ LightBulb.hs
@@ -1,8 +1,11 @@
 module LightBulb where
 
 import Data.Char
+import Data.IORef
 import Network.Socket
 import Graphics.Gloss.Interface.IO.Animate
+import Control.Monad (forever, void)
+import Control.Concurrent
 
 port :: PortNumber
 port = 9876  
@@ -14,10 +17,14 @@ type Host = SockAddr
 
 main :: IO ()
 main = withSocketsDo $ do
-  s <- socket AF_INET Datagram defaultProtocol
+  s        <- socket AF_INET Datagram defaultProtocol
   bindAddr <- inet_addr host
   bindSocket s (SockAddrInet port bindAddr)
-  animateFixedIO (InWindow "LightBulb" (500,500) (20,20)) (makeColor 0 0 0 1) $ \_ -> do
+  colorRef <- newIORef $ makeColor8 0 0 0 1
+  void $ forkIO $ forever $ do
     (msg, _, _) <- recvFrom s 1024
     let (r:g:b:_) = map ord msg
-    return $ color (makeColor8 r g b 255)$ rectangleSolid 500 500
+    writeIORef colorRef $ makeColor8 r g b 255
+  animateFixedIO (InWindow "LightBulb" (500,500) (20,20)) (makeColor 0 0 0 1) $ \_ -> do
+    c <- readIORef colorRef
+    return $ color c $ rectangleSolid 500 500
