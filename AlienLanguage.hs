70c53d29bc0c3b31e51582a7e98334a7e7166391
Peter Marks <peter@indigomail.net>
2013-08-08 19:15:48 +0100

    implemented output

--- AlienLanguage.hs
+++ AlienLanguage.hs
@@ -1,7 +1,12 @@
 module AlienLanguage where
 
+import Text.Printf
+
 main :: IO ()
-main = interact $ output . uncurry process . parse
+main = interact go
+
+go :: String -> String
+go = output . uncurry process . parse
 
 parse :: String -> ([String], [String])
 parse content =
@@ -10,7 +15,7 @@ parse content =
   in splitAt dictionarySize body
 
 process :: [String] -> [String] -> [Int]
-process = undefined
+process dictionary patterns = [ length dictionary, length patterns ]
 
 output :: [Int] -> String
-output = undefined
+output = unlines . zipWith (printf "Case #%d: %d") [(1::Int)..]
