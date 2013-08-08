dbc918cdde880a8253c9ec4ed1e335369b637afe
Peter Marks <peter@indigomail.net>
2013-08-08 20:40:35 +0100

    Rewritten to use bit patterns

--- AlienLanguage.hs
+++ AlienLanguage.hs
@@ -1,7 +1,8 @@
 module AlienLanguage where
 
 import Text.Printf
-import Text.Regex.Posix
+import Data.Bits
+import Data.Char
 
 main :: IO ()
 main = interact go
@@ -16,16 +17,26 @@ parse content =
   in splitAt dictionarySize body
 
 process :: [String] -> [String] -> [Int]
-process dictionary = map (testPattern dictionary)
-
-testPattern :: [String] -> String -> Int
-testPattern dictionary pattern = length $ filter (=~ pattern') dictionary
+process dictionary = map (testPattern dictionary' . patternToBits)
   where
-    pattern' = map f pattern
-    f '(' = '['
-    f ')' = ']'
-    f c   = c
-    
+    dictionary' = map wordToBits dictionary
+
+testPattern :: [[Int]] -> [Int] -> Int
+testPattern dictionary pattern = length $ filter (found pattern) dictionary
+
+found :: [Int] -> [Int] -> Bool
+found pattern = notElem 0 . zipWith (.&.) pattern
+
+charToBits :: Char -> Int
+charToBits = bit . subtract (ord 'a') . ord
+
+wordToBits :: String -> [Int]
+wordToBits = map charToBits
+
+patternToBits :: String -> [Int]
+patternToBits [] = []
+patternToBits ('(':cs) = let (alts, _:cs') = break (==')')cs in sum (map charToBits alts) : patternToBits cs'
+patternToBits (c:cs) = charToBits c : patternToBits cs
 
 output :: [Int] -> String
 output = unlines . zipWith (printf "Case #%d: %d") [(1::Int)..]
