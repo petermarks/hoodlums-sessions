edfed1cae62b9db911003a09285e7a2acb6ba89f
Peter Marks <peter@indigomail.net>
2014-01-09 20:15:37 +0000

    Introduced state monad

--- synacor.hs
+++ synacor.hs
@@ -1,22 +1,33 @@
-{-# language RecordWildCards #-}
-
 module Synacor where
 
+import Control.Applicative
+import Control.Monad.State.Strict
 import qualified Data.IntMap as M
 import Data.Word
 import Data.Char
 
-data State = State
+data Machine = Machine
   { mem   :: !(M.IntMap Word16)
   , stack :: ![Word16]
   , ip    :: !Int
   }
 
-run :: State -> String -> String
-run s@State{..} input = case  mem M.! ip of
-  0  -> []
-  19 -> chr (fromIntegral (mem M.! (ip+1))) : run s{ip = ip + 2} input -- Need to handle registers too
-  21 -> run s{ip = ip + 1} input
+type Syn = State Machine
 
-load :: [Word16] -> State
-load prog = State (M.fromList $ zip [0..] prog) [] 0
+run :: Machine -> String -> String
+run m i = evalState (run' i) m
+
+run' :: String -> Syn String 
+run' input = getMem 0 >>= \inst -> case inst of
+  0  -> return []
+  19 -> (:) <$> (chr . fromIntegral <$> getMem 1) <*> (incip 2 >> run' input) -- Need to handle registers too
+  21 -> incip 1 >> run' input
+
+incip :: Int -> Syn ()
+incip n = modify $ \s -> s{ip = ip s + n}
+
+getMem :: Int -> Syn Word16
+getMem offset = (M.!) <$> gets mem <*> gets ((+ offset) . ip)
+
+load :: [Word16] -> Machine
+load prog = Machine (M.fromList $ zip [0..] prog) [] 0
