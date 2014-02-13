e7cc5a2309ea2c87f2d915f9910324aa782a0031
Peter Marks <peter@indigomail.net>
2014-02-13 19:30:13 +0000

    Implemented processing

--- listUpdate.hs
+++ listUpdate.hs
@@ -16,3 +16,20 @@ updates =
   , (1004, Delete)
   , (1006, SetTrue)]
 
+processNaive :: [(Int, Operation)] -> [(Int, Bool)] -> [(Int, Bool)]
+processNaive us xs = foldr processOne xs us
+
+processOne :: (Int, Operation) -> [(Int, Bool)] -> [(Int, Bool)]
+processOne (i, Delete)  = filter ((/= i) . fst)
+processOne (i, SetTrue) = map (\(j, b) -> if i == j then (j, True) else (j, b))
+
+process :: [(Int, Operation)] -> [(Int, Bool)] -> [(Int, Bool)]
+process _                []          = []
+process []               xs          = xs
+process us@((i, op):us') xs@((j, b):xs') = case i `compare` j of
+  LT -> process us' xs
+  GT -> (j, b) : process us xs'
+  EQ -> case op of
+    Delete  -> process us' xs'
+    SetTrue -> (j, True) : process us' xs'
+
