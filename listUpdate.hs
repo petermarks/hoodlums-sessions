51419b3474b8efb782b35e64172a3b76cf44a08b
Peter Marks <peter@indigomail.net>
2014-02-13 20:07:29 +0000

    Added quickCheck test

--- listUpdate.hs
+++ listUpdate.hs
@@ -1,6 +1,15 @@
 module ListUpdate where
 
+import Test.QuickCheck
+import Data.List
+import Data.Function
+import Data.Ord
+
 data Operation = Delete | SetTrue
+  deriving (Eq, Enum, Bounded, Show)
+
+instance Arbitrary Operation where
+  arbitrary = arbitraryBoundedEnum
 
 original :: [(Int, Bool)]
 original = 
@@ -33,3 +42,11 @@ process us@((i, op):us') xs@((j, b):xs') = case i `compare` j of
     Delete  -> process us' xs'
     SetTrue -> (j, True) : process us' xs'
 
+prop_process_matches_naive :: [(Int, Operation)] -> [(Int, Bool)] -> Bool
+prop_process_matches_naive us xs = processNaive us' xs' == process us' xs'
+  where
+    us' = nubBy ((==) `on` fst) . sortBy (comparing fst) $ us
+    xs' = nubBy ((==) `on` fst) . sortBy (comparing fst) $ xs
+
+main :: IO ()
+main = quickCheck prop_process_matches_naive
