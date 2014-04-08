c1b5c884df7eca8bad6ea7c1cdd3a359fc098331
Peter Marks <peter@indigomail.net>
2014-04-08 18:51:35 +0100

    Added a class

--- DamageDice.hs
+++ DamageDice.hs
@@ -1,31 +1,48 @@
+{-# language TypeSynonymInstances, FlexibleInstances #-}
+
 module DamageDice where
 
 import System.Random
 import Control.Monad.State
 import Control.Applicative
 
+
+-------------------------------------------------------------
+-- Interpreter class
+
+class Interpreter i where
+  roll :: Int -> i
+  mult :: Int -> i -> i
+  add  :: i -> i -> i
+  lit  :: Int -> i
+
+
+-------------------------------------------------------------
+-- Roller instance
+
 type Roller = State StdGen
 
-runRoller :: Roller a -> StdGen -> a
+runRoller :: Roller Int -> StdGen -> Int
 runRoller = evalState
 
-roll :: Int -> Roller Int
-roll x = do
-  gen <- get
-  let (n, gen') = randomR (1, x) gen
-  put gen'
-  return n
+instance Interpreter (Roller Int) where
+  roll x = do
+    gen <- get
+    let (n, gen') = randomR (1, x) gen
+    put gen'
+    return n
 
-mult :: Int -> Roller Int -> Roller Int
-mult x r = sum <$> replicateM x r
+  mult x r = sum <$> replicateM x r
 
-add :: Roller Int -> Roller Int -> Roller Int
-add = liftA2 (+)
+  add = liftA2 (+)
 
-lit :: Int -> Roller Int
-lit = pure
+  lit = pure
 
-test :: Roller Int
+
+-------------------------------------------------------------
+-- Main
+
+test :: (Interpreter i) => i
 test = mult 2 (roll 6 ) `add` lit 17
 
 main :: IO ()
