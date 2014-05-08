7d7c86172de970fed01d0c9b4c9127a3fc89fae4
Peter Marks <peter@indigomail.net>
2014-05-08 19:20:47 +0100

    Added integers

--- Threads.hs
+++ Threads.hs
@@ -1,4 +1,5 @@
-{-# language GADTs, KindSignatures #-}
+{-# language TypeSynonymInstances, FlexibleInstances, 
+      GADTs, KindSignatures #-}
 
 module Threads where
 
@@ -6,17 +7,27 @@ newtype Thread a = Thread {runThread :: IO a}
 
 data V :: * -> * where
   VString :: String -> V String
+  VInt    :: Int    -> V Int
 
-v :: String -> V String
-v = VString
+class ToV a where
+  v :: a -> V a
+
+instance ToV String where
+  v = VString
+
+instance ToV Int where
+  v = VInt
 
 out :: V String -> Thread ()
 out (VString s) = Thread $ putStrLn s
 
+toString :: V Int -> V String
+toString (VInt i) = VString $ show i
+
 ------------------------------------------------------------
 
 test :: Thread ()
-test = out $ v "Hello"
+test = out $ toString $ v 123
 
 main :: IO ()
 main = runThread test
