2406e5bbc8733f57c5d58a0087ead5e98a52a836
Peter Marks <peter@indigomail.net>
2014-12-11 20:35:16 +0000

    Better train representation, but still slow

--- trains.hs
+++ trains.hs
@@ -1,9 +1,14 @@
 module Trains where
 
+import Control.Monad
 import Data.Char
 import Data.List
+import Data.Maybe
 import qualified Data.Set as S
 import Text.Printf
+
+data Train = Train Char (S.Set Char) Char
+  deriving (Show)
 
 main :: IO ()
 main = interact go
@@ -18,11 +23,25 @@ output :: [Int] -> String
 output = unlines . zipWith (printf "Case #%d: %d") [(1::Int)..]
 
 process :: [String] -> Int
-process trains= length
-  [ ()
-  | ordering <- permutations trains
-  , let combined = concat ordering
-  , numUnique == length (group combined)
-  ]
+process = maybe 0 (length . mapMaybe check . permutations) . mapM train
   where
-    numUnique = S.size (S.fromList (concat trains))
+    check :: [Train] -> Maybe Train
+    check (t:ts) = foldM couple t ts
+    check []     = Nothing
+
+train :: String -> Maybe Train
+train []     = Nothing
+train [c]    = Just $ Train c (S.singleton c) c
+train (c:cs) = train cs >>= coupleCar
+  where
+    coupleCar tt@(Train l s r) 
+      | l == c         = Just tt
+      | c `S.member` s = Nothing
+      | otherwise      = Just $ Train c (S.insert c s) r
+
+couple :: Train -> Train -> Maybe Train
+couple (Train l1 s1 r1) (Train l2 s2 r2)
+  | S.null s || (r1 == l2 && s == S.singleton r1) = Just $ Train l1 (s1 `S.union` s2) r2
+  | otherwise                                     = Nothing
+  where
+    s = s1 `S.intersection` s2
