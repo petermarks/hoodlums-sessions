623ffde5c05d1ee448f3e2e9769cb0b3a986929e
Peter Marks <peter@indigomail.net>
2018-01-18 20:32:50 +0000

    Part 2 of RecursiveCircus - partial

--- RecursiveCircus.hs
+++ RecursiveCircus.hs
@@ -2,34 +2,52 @@ module RecursiveCircus where
 
 import Data.Set (Set)
 import qualified Data.Set as S
---import Data.Monoid
---import Data.Foldable
---import Control.Applicative
-import Control.Arrow
+import Data.Map (Map)
+import qualified Data.Map as M
+import Data.List (nub)
 
-type Node = (String, Set String)
+type Node = (String, Int, Set String)
 
 type Tower = (Set String, Set String)
 
-solve :: String -> String
-solve = pickRoot . setify . map splitUp . lines
+type WeightMap = Map String Int
+
+solveRoot :: [Node] -> String
+solveRoot = pickRoot . setify 
+
+parse :: String -> [Node]
+parse = map splitUp . lines
 
 splitUp :: String -> Node
 splitUp = f . words . filter (/= ',')
   where
     f :: [String] -> Node
-    f (p : _ : _ : cs) = (p, S.fromList cs)
-    f (p : _) = (p, mempty)
+    f (p : w : _ : cs) = (p, fst $ head $ reads $ tail w, S.fromList cs)
+    f (p : w : _) = (p, fst $ head $ reads $ tail w, mempty)
     f l = error $ "invalid line: " ++ unwords l
 
 setify :: [Node] -> Tower
-setify = foldMap (first S.singleton)
+setify = foldMap (\(p, _, cs) -> (S.singleton p, cs))
 
 pickRoot :: Tower -> String
 pickRoot = S.findMin . uncurry S.difference
 
+buildWeightMap :: [Node] -> WeightMap
+buildWeightMap ns = wm
+  where
+    wm = M.fromList $ map f ns
+    f (p, w, cs) = (p, w + sum (fmap (wm M.!) (S.toList cs)))
+
+isImbalanced :: WeightMap -> Node -> Bool
+isImbalanced wm (_, _, cs) = (> 1) $ length $ nub $ map (wm M.!) $ S.toList cs
+
 main :: IO ()
 main = do
   f <- readFile "input.txt"
-  let ans = solve f
-  putStrLn ans 
+  let parsed = parse f
+  let root = solveRoot parsed
+  let wm = buildWeightMap parsed
+  let imbalances = filter (isImbalanced wm) parsed
+  putStrLn root
+  print $ wm M.! root
+  print imbalances
